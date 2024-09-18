1268575d60b039ed60ea3803c91481676a6250db
venkatakumar <kumar.venkata9@googlemail.com>
2024-09-18 14:56:05 +0100

    Added Simple Search Test develop branch created

--- PlaywrightTests/UnitTest1.cs
+++ PlaywrightTests/UnitTest1.cs
@@ -1,15 +1,20 @@
+using System.Text.RegularExpressions;
+using Microsoft.Playwright.NUnit;
+
 namespace PlaywrightTests;
 
-public class Tests 
+public class Tests : PageTest
 {
     [SetUp]
-    public void Setup()
+    public async Task Setup()
     {
+        await Page.GotoAsync("https://app-planningregister-planningportal.pp.tqinfra.co.uk/simple-search");
     }
 
     [Test]
-    public void Test1()
+    public async Task SimpleSearchTest()
     {
-        Assert.Pass();
+        await Expect(Page).ToHaveTitleAsync(new Regex("Northern Ireland Public Register"));
+        await Page.ClickAsync(selector:"text=Advanced search");
     }
 }

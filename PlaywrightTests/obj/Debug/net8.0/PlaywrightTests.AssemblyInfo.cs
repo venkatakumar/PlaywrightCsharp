fb387908bb25cf4dc42d87c2e5f4b60809471674
Venkata.dhanalakota <venkata.dhanalakota@terraquest.co.uk>
2024-09-27 16:32:25 +0100

    Updated simple search test

--- PlaywrightTests/obj/Debug/net8.0/PlaywrightTests.AssemblyInfo.cs
+++ PlaywrightTests/obj/Debug/net8.0/PlaywrightTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f824348594b21f5d2af5830048718cc59134475")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f89780847c36d9c121182271775ed8e32cb273f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

d480834d37036c0aa99df86004afed8d435efed6
Venkata.dhanalakota <venkata.dhanalakota@terraquest.co.uk>
2024-10-16 21:46:28 +0100

    added advanced search test

--- PlaywrightTests/TestData/SampleTestData.cs
+++ PlaywrightTests/TestData/SampleTestData.cs
@@ -9,5 +9,6 @@ namespace PlaywrightTests.TestData
     {
         public const string Authority = "Belfast City Council";
         public const string SearchTerm = "bt1";
+        public const string AdvancedSearch_RefNumer = "la04";
     }
 }
